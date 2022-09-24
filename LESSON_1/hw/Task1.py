053ed94c0427b1e11b5122ae1877706a4bd22c8e
ol-kis <99480768+ol-kis@users.noreply.github.com>
2022-09-24 11:26:38 +0300

    Update Task1.py

--- LESSON_1/hw/Task1.py
+++ LESSON_1/hw/Task1.py
@@ -1,13 +1,13 @@
 count=0
 num=input("enter number: ")
-list=["понедельник","вторник","среда","четверг","пятница","суббота","воскресение"]
+list=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 while count==0:            
     if num.isdigit()==True and int(num)<7 and not int(num)<1:
         if int(num)<6:
             #print("working day")
             print(f' day of the week "{list[int(num)-1]}" is working day')
         else:
-            print("weekend")  
+            rint(f' day of the week "{list[int(num)-1]}" is weekend')  
         count=1
     
     else: 
