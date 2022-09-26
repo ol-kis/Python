7ba7cdfabfb22f74dbc0f34448a73bad0f54203b
ol-kis <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>
2022-09-26 20:11:18 +0300

    HomeWork 2

--- LESSON_1/hw/Task1.py
+++ LESSON_1/hw/Task1.py
@@ -1,17 +1,19 @@
 count=0
 num=input("enter number: ")
+list=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 while count==0:            
-    if num.isdigit()==True and int(num)<7 and not int(num)<0:
+    if num.isdigit()==True and int(num)<7 and not int(num)<1:
         if int(num)<6:
-            print("working day")
+            #print("working day")
+            print(f' day of the week "{list[int(num)-1]}" is working day')
         else:
-            print("weekend")  
+            print(f' day of the week "{list[int(num)-1]}" is weekend')
         count=1
     
     else: 
         print("you entered not number or wrong number")
         num=input("enter number again: ")
         
-       
+      
         
         
